612397c8605a30915a26673369806cd90ab16a02
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2017-11-15 16:58:22 +0200

    Fix getting version if cmake is done out of source folder
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- generateVersion.cmake
+++ generateVersion.cmake
@@ -1,10 +1,12 @@
 set(GIT_VERSION "undefined")
 
 execute_process(
-	COMMAND
-	${GIT_EXECUTABLE} describe --tags --always --abbrev=4 --dirty
-    	OUTPUT_VARIABLE GIT_VERSION
+	WORKING_DIRECTORY ${GIT_SOURCE_DIR}
+	COMMAND ${GIT_EXECUTABLE} describe --tags --always --abbrev=4 --dirty
+	OUTPUT_VARIABLE GIT_VERSION
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
+message(STATUS "VERSION: ${GIT_VERSION}")
 
 configure_file(${INPUT_FILE} ${OUTPUT_FILE})
